ad7582bc14b8ba09c98d57e24c5651282bff5527
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-05 17:37:17 -0400

    duplicate dropdown for instructional purposes.

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -33,6 +33,20 @@ class ThemeSwitcher extends Component {
 						<a class="dropdown-item" href="#" onClick={this.resetTheme}>Default Theme</a>
 					</div>
 				</div>
+				<span className={`h1 mb-4 w-100 text-center text-${themeClass}`}>{ theme || 'Default' }</span>
+				<div class="btn-group">
+					<button type="button" class={`btn btn-${themeClass} btn-lg`}>{ theme || 'Choose' } Theme</button>
+					<button type="button" class={`btn btn-${themeClass} btn-lg dropdown-toggle dropdown-toggle-split`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<span class="sr-only">Toggle Theme Dropdown</span>
+					</button>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Primary', e)}>Primary Theme</a>
+						<a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Danger', e)}>Danger Theme</a>
+						<a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Success', e)}>Success Theme</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#" onClick={this.resetTheme}>Default Theme</a>
+					</div>
+				</div>
 			</div>
     );
   }
