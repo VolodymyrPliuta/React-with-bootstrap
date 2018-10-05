697a569c597aa82e7e8be655d5aff96ec45a18f0
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-05 18:18:11 -0400

    remove data-parent attribute and collapsed class so expanded work correctly

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -62,7 +62,7 @@ class ThemeSwitcher extends Component {
 					</h5>
 				</div>
 
-				<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+				<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" >
 					<div class="card-body">
 						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 					</div>
@@ -71,12 +71,12 @@ class ThemeSwitcher extends Component {
 			<div class="card">
 				<div class="card-header" id="headingTwo">
 					<h5 class="mb-0">
-						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+						<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
 							Collapsible Group Item #2
 						</button>
 					</h5>
 				</div>
-				<div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
+				<div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" >
 					<div class="card-body">
 						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 					</div>
@@ -85,12 +85,12 @@ class ThemeSwitcher extends Component {
 			<div class="card">
 				<div class="card-header" id="headingThree">
 					<h5 class="mb-0">
-						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+						<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
 							Collapsible Group Item #3
 						</button>
 					</h5>
 				</div>
-				<div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent="#accordionExample">
+				<div id="collapseThree" class="collapse show" aria-labelledby="headingThree" >
 					<div class="card-body">
 						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 					</div>
