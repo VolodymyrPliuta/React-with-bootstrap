ce55b7340bc9332e58668aa15f0174f1acf6449b
americanman <snowboardracer7@gmail.com>
2018-10-06 18:04:47 -0400

    Pass header/body props

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
 const CustomCardItem = ( { cardNumber, nextButton, header, body} ) => {
+  console.log( { cardNumber, nextButton, header} )
   return (
     <div className="card">
       <div className="card-header" id="headingOne">
         <h5 className="mb-0">
           <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne">
-            Collapsible Group Item #1
+            { header }
           </button>
         </h5>
       </div>
 
       <div id="collapseOne" className="collapse show" aria-labelledby="headingThree" >
         <div className="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          { body }
         </div>
         <button className="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
             Next
@@ -47,7 +48,7 @@ class ThemeSwitcher extends Component {
         <div className="accordion" id="accordionExample">
                     
             <CustomCardItem
-              cardNumber="One"
+              cardNumber="1"
               nextButton="true"
               header="Collapsible Group Item #1"
               body="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
