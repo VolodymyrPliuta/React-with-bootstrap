24ceb3fb1c55bfd40bd96874dfd285c4ca81d078
americanman <snowboardracer7@gmail.com>
2018-10-06 17:34:52 -0400

    Replace class with className

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -19,7 +19,7 @@ class ThemeSwitcher extends Component {
 
     return (
       <div>
-        <div class="accordion" id="accordionExample">
+        <div className="accordion" id="accordionExample">
           {/*
             <CustomCardItem
               cardNumber="One"
@@ -32,68 +32,68 @@ class ThemeSwitcher extends Component {
               Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
             />
           */}
-          <div class="card">
-            <div class="card-header" id="headingOne">
-              <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne">
+          <div className="card">
+            <div className="card-header" id="headingOne">
+              <h5 className="mb-0">
+                <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne">
                   Collapsible Group Item #1
                 </button>
               </h5>
             </div>
 
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingThree" >
-              <div class="card-body">
+            <div id="collapseOne" className="collapse show" aria-labelledby="headingThree" >
+              <div className="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
+              <button className="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
                   Next
               </button>
             </div>
           </div>
-          <div class="card">
-            <div class="card-header" id="headingTwo">
-              <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
+          <div className="card">
+            <div className="card-header" id="headingTwo">
+              <h5 className="mb-0">
+                <button className="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
                   Collapsible Group Item #2
                 </button>
               </h5>
             </div>
-            <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" >
-              <div class="card-body">
+            <div id="collapseTwo" className="collapse show" aria-labelledby="headingTwo" >
+              <div className="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree">
+              <button className="btn btn-link" data-toggle="collapse" data-target="#collapseThree">
                 Next
               </button>
             </div>
           </div>
-          <div class="card">
-            <div class="card-header" id="headingThree">
-              <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree">
+          <div className="card">
+            <div className="card-header" id="headingThree">
+              <h5 className="mb-0">
+                <button className="btn btn-link" data-toggle="collapse" data-target="#collapseThree">
                   Collapsible Group Item #3
                 </button>
               </h5>
             </div>
-            <div id="collapseThree" class="collapse show" aria-labelledby="headingThree" >
-              <div class="card-body">
+            <div id="collapseThree" className="collapse show" aria-labelledby="headingThree" >
+              <div className="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour">
+              <button className="btn btn-link" data-toggle="collapse" data-target="#collapseFour">
                 Next
               </button>
             </div>
           </div>
-          <div class="card">
-            <div class="card-header" id="headingFour">
-              <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour">
+          <div className="card">
+            <div className="card-header" id="headingFour">
+              <h5 className="mb-0">
+                <button className="btn btn-link" data-toggle="collapse" data-target="#collapseFour">
                   Collapsible Group Item #4
                 </button>
               </h5>
             </div>
-            <div id="collapseFour" class="collapse show" aria-labelledby="headingThree" >
-              <div class="card-body">
+            <div id="collapseFour" className="collapse show" aria-labelledby="headingThree" >
+              <div className="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
             </div>
@@ -103,31 +103,31 @@ class ThemeSwitcher extends Component {
         <br/>
         <div className="d-flex flex-wrap justify-content-center position-relative w-100 h-1-0 align-items-center align-content-center">
           <span className={`h1 mb-4 w-100 text-center text-${themeClass}`}>{ theme || 'Default' }</span>
-          <div class="btn-group">
-            <button type="button" class={`btn btn-${themeClass} btn-lg`}>{ theme || 'Choose' } Theme</button>
-            <button type="button" class={`btn btn-${themeClass} btn-lg dropdown-toggle dropdown-toggle-split`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="sr-only">Toggle Theme Dropdown</span>
+          <div className="btn-group">
+            <button type="button" className={`btn btn-${themeClass} btn-lg`}>{ theme || 'Choose' } Theme</button>
+            <button type="button" className={`btn btn-${themeClass} btn-lg dropdown-toggle dropdown-toggle-split`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span className="sr-only">Toggle Theme Dropdown</span>
             </button>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Primary', e)}>Primary Theme</a>
-              <a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Danger', e)}>Danger Theme</a>
-              <a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Success', e)}>Success Theme</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#" onClick={this.resetTheme}>Default Theme</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#" onClick={e => this.chooseTheme('Primary', e)}>Primary Theme</a>
+              <a className="dropdown-item" href="#" onClick={e => this.chooseTheme('Danger', e)}>Danger Theme</a>
+              <a className="dropdown-item" href="#" onClick={e => this.chooseTheme('Success', e)}>Success Theme</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#" onClick={this.resetTheme}>Default Theme</a>
             </div>
           </div>
           <span className={`h1 mb-4 w-100 text-center text-${themeClass}`}>{ theme || 'Default' }</span>
-          <div class="btn-group">
-            <button type="button" class={`btn btn-${themeClass} btn-lg`}>{ theme || 'Choose' } Theme</button>
-            <button type="button" class={`btn btn-${themeClass} btn-lg dropdown-toggle dropdown-toggle-split`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="sr-only">Toggle Theme Dropdown</span>
+          <div className="btn-group">
+            <button type="button" className={`btn btn-${themeClass} btn-lg`}>{ theme || 'Choose' } Theme</button>
+            <button type="button" className={`btn btn-${themeClass} btn-lg dropdown-toggle dropdown-toggle-split`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span className="sr-only">Toggle Theme Dropdown</span>
             </button>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Primary', e)}>Primary Theme</a>
-              <a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Danger', e)}>Danger Theme</a>
-              <a class="dropdown-item" href="#" onClick={e => this.chooseTheme('Success', e)}>Success Theme</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#" onClick={this.resetTheme}>Default Theme</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#" onClick={e => this.chooseTheme('Primary', e)}>Primary Theme</a>
+              <a className="dropdown-item" href="#" onClick={e => this.chooseTheme('Danger', e)}>Danger Theme</a>
+              <a className="dropdown-item" href="#" onClick={e => this.chooseTheme('Success', e)}>Success Theme</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#" onClick={this.resetTheme}>Default Theme</a>
             </div>
           </div>
         </div>
