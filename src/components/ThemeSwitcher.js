89d08704e2979a27b67bc250d66aa714d3849008
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-06 17:12:10 -0400

    Simplify button and have collapse own card.

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -20,15 +20,21 @@ class ThemeSwitcher extends Component {
     return (
       <div>
         <div class="accordion" id="accordionExample">
+          {/*
+            <CustomCardItem
+              cardNumber="One"
+              header="Collapsible Group Item #1"
+              body="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
+              3 wolf moon officia aute, non cupidatat skateboard dolor brunch. 
+              Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. 
+              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. 
+              Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
+            />
+          */}
           <div class="card">
             <div class="card-header" id="headingOne">
               <h5 class="mb-0">
-                <button class="btn btn-link"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapseThree"
-                  aria-expanded="true"
-                  aria-controls="collapseOne">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne">
                   Collapsible Group Item #1
                 </button>
               </h5>
@@ -43,12 +49,7 @@ class ThemeSwitcher extends Component {
           <div class="card">
             <div class="card-header" id="headingTwo">
               <h5 class="mb-0">
-                <button class="btn btn-link"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapseFour"
-                  aria-expanded="true"
-                  aria-controls="collapseTwo">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
                   Collapsible Group Item #2
                 </button>
               </h5>
@@ -62,11 +63,7 @@ class ThemeSwitcher extends Component {
           <div class="card">
             <div class="card-header" id="headingThree">
               <h5 class="mb-0">
-                <button class="btn btn-link"
-                  type="button" data-toggle="collapse"
-                  data-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree">
                   Collapsible Group Item #3
                 </button>
               </h5>
@@ -80,11 +77,7 @@ class ThemeSwitcher extends Component {
           <div class="card">
             <div class="card-header" id="headingFour">
               <h5 class="mb-0">
-                <button class="btn btn-link"
-                  type="button" data-toggle="collapse"
-                  data-target="#collapseTwo"
-                  aria-expanded="true"
-                  aria-controls="collapseFour">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour">
                   Collapsible Group Item #4
                 </button>
               </h5>
