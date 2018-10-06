538b6e69aacfdc8c33d3a72c326c0bd8d6236270
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-06 17:18:20 -0400

    Next button inside card opens and closes next card

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -23,6 +23,7 @@ class ThemeSwitcher extends Component {
           {/*
             <CustomCardItem
               cardNumber="One"
+              nextButton="true"
               header="Collapsible Group Item #1"
               body="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
               3 wolf moon officia aute, non cupidatat skateboard dolor brunch. 
@@ -44,6 +45,9 @@ class ThemeSwitcher extends Component {
               <div class="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo">
+                  Next
+              </button>
             </div>
           </div>
           <div class="card">
@@ -58,6 +62,9 @@ class ThemeSwitcher extends Component {
               <div class="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree">
+                Next
+              </button>
             </div>
           </div>
           <div class="card">
@@ -72,6 +79,9 @@ class ThemeSwitcher extends Component {
               <div class="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
               </div>
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour">
+                Next
+              </button>
             </div>
           </div>
           <div class="card">
