fab9380c1338f6a0d12c14c83ae9a3803b02e4b9
americanman <snowboardracer7@gmail.com>
2018-10-06 18:39:49 -0400

    Remove duplicate code using component

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -46,66 +46,39 @@ class ThemeSwitcher extends Component {
       <div>
         <div className="accordion" id="accordionExample">
                     
-            <CustomCardItem
-              cardNumber="1"
-              nextButton="true"
-              header="Collapsible Group Item #1"
-              body="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
-              3 wolf moon officia aute, non cupidatat skateboard dolor brunch. 
-              Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. 
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. 
-              Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
-            />
+          <CustomCardItem
+            cardNumber="1"
+            nextButton="true"
+            header="Collapsible Group Item #1"
+            body="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
+            3 wolf moon officia aute, non cupidatat skateboard dolor brunch. 
+            Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. 
+            Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. 
+            Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
+          />
           
-          
-          <div className="card">
-            <div className="card-header" id="heading2">
-              <h5 className="mb-0">
-                <button className="btn btn-link" data-toggle="collapse" data-target="#collapse2">
-                  Collapsible Group Item #2
-                </button>
-              </h5>
-            </div>
-            <div id="collapse2" className="collapse show" aria-labelledby="heading2" >
-              <div className="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </div>
-              <button className="btn btn-link" data-toggle="collapse" data-target="#collapse3">
-                Next
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-header" id="heading3">
-              <h5 className="mb-0">
-                <button className="btn btn-link" data-toggle="collapse" data-target="#collapse3">
-                  Collapsible Group Item #3
-                </button>
-              </h5>
-            </div>
-            <div id="collapse3" className="collapse show" aria-labelledby="heading3" >
-              <div className="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </div>
-              <button className="btn btn-link" data-toggle="collapse" data-target="#collapse4">
-                Next
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-header" id="heading4">
-              <h5 className="mb-0">
-                <button className="btn btn-link" data-toggle="collapse" data-target="#collapse4">
-                  Collapsible Group Item #4
-                </button>
-              </h5>
-            </div>
-            <div id="collapse4" className="collapse show" aria-labelledby="heading3" >
-              <div className="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div>
+          <CustomCardItem
+            cardNumber="2"
+            nextButton="true"
+            header="Collapsible Group Item #2"
+            body="Body for card number 2."
+          />
+
+          <CustomCardItem
+            cardNumber="3"
+            nextButton="true"
+            header="Collapsible Group Item #3"
+            body="Did you know, one in five college graduates are named fred?"
+          />
+
+         <CustomCardItem
+            cardNumber="4"
+            nextButton="false"
+            header="Collapsible Group Item #4"
+            body="body 4 is done"
+          />
+
+
         </div>
         <br/>
         <br/>
