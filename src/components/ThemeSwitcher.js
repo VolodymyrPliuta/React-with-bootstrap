3bad19304bb31ba9a06b173142d3607e84d68194
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-09 22:03:01 -0400

    next button shrink current card.

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -18,6 +18,7 @@ class ThemeSwitcher extends Component {
   nextButtonAccordianItems( cardNumber ) {
     const newItems = [...this.state.arrayOfCards];
     const newNum = +cardNumber+1
+    newItems[cardNumber] = " "
     newItems[newNum] = (newItems[newNum] === "show")
       ? newItems[newNum] = "show"
       : newItems[newNum] = "show"
